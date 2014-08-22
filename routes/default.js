9702284cbc89f5354a506d895c094462a2df4b41
Nate Clark <nclark@knotice.com>
2014-08-22 16:29:07 -0400

    Fixed trophy history route

--- routes/default.js
+++ routes/default.js
@@ -11,7 +11,7 @@ module.exports = function(app, options){
     { "method": "get",  "path" : "/heartbeat",                                      "controller" : "pageRouteController", "action" : "Heartbeat" },
     { "method": "get",  "path" : "/:leagueId",                                      "controller" : "apiRouteController",  "action" : "Info" },
     { "method": "get",  "path" : "/:leagueId/trophies",                             "controller" : "apiRouteController",  "action" : "Trophies" },
-    { "method": "get",  "path" : "/:leagueId/trophy/history/:trophyId",             "controller" : "apiRouteController",  "action" : "TrophyHistory" },
+    { "method": "get",  "path" : "/:leagueId/trophies/history/:trophyId",           "controller" : "apiRouteController",  "action" : "TrophyHistory" },
     { "method": "get",  "path" : "/:leagueId/:seasonId",                            "controller" : "apiRouteController",  "action" : "Info" },
     { "method": "get",  "path" : "/:leagueId/:seasonId/members",                    "controller" : "apiRouteController",  "action" : "Members" },
     { "method": "get",  "path" : "/:leagueId/:seasonId/draftrecap",                 "controller" : "apiRouteController",  "action" : "DraftRecap" },
